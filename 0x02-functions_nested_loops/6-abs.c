f54c3019b456a7423d79411bdef5f0aa9234a5c2
AMO15310 <wachiraamos402@gmail.com>
2022-12-15 00:24:39 +0300

    Convert to absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int_abs - function to convert a number to its absolute value
+ * _abs - function to convert a number to its absolute value
  * @a:  is the int that will use for the argument of the function
  * Return: 0
  */
@@ -10,7 +10,7 @@ int _abs(int a)
 {
 	if (a > 0 || a == 0)
 	{
-		return a;
+		return (a);
 	}
 	else
 		return (a * -1);
