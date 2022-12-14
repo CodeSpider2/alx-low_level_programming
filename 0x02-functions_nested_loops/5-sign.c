034cb04705688fb6a8c8c70d381a1331a014378f
AMO15310 <wachiraamos402@gmail.com>
2022-12-15 00:15:36 +0300

    Check the sighn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign  - function to check whether a number is positive or negative
- * @c: is the int that will use for the argument of the function
+ * @n: is the int that will use for the argument of the function
  * Return: 0
  */
 int print_sign(int n)
