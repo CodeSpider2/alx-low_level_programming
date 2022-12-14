d4c1eba59e019fd9ca310777233d81a533a3256f
AMO15310 <wachiraamos402@gmail.com>
2022-12-14 23:22:28 +0300

    print it 10 times !

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char a;
 	int i ;
 
-	for (a = 'a'; a <= 'z'; a++)
+	for (a = 0; a < 10; i++)
 	{
-	for(i = 0; i < 10; i++) 
+	for(a = 'a'; a <= 'z'; a++) 
 	{
 		_putchar(a);
 	}
