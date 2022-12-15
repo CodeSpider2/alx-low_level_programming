fc1e8e26cce00cee7661605a29c8e883826e07cb
AMO15310 <wachiraamos402@gmail.com>
2022-12-15 14:50:57 +0300

    Print all except for 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	int num = 0;
+	int num;
 
-	while (num < 10)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
 			_putchar((num % 10) + '0');
-			num++;
 	}
+
 	_putchar('\n');
 }
 
