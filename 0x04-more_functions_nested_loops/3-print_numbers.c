a9b1f523b32be8e08ccd87458d0b849095c6d778
AMO15310 <wachiraamos402@gmail.com>
2022-12-15 14:32:46 +0300

    Print from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,10 @@
  */
 void print_numbers(void)
 {
-	int i = 0;
+	int num;
 
-	while(i<10)
-	{
-		_putchar((i % 10) + '0');
-		i++;
-	}
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
 	_putchar('\n');
 }
