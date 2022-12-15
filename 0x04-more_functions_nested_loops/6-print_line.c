64ea7a4910822eaab87196965e50b613b15ccddc
AMO15310 <wachiraamos402@gmail.com>
2022-12-15 15:08:41 +0300

    Print a straight horizontal on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using the character _.
+ * @n: The number of  characters to be printed.
+ */
+void print_line(int n)
+{
+
+	if (n > 0)
+	{
+		for(n = 0; n < 0; n++ )
+			_putchar('-');
+		_putchar('\n');
+	}
+}
