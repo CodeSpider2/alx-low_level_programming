95a96544ae3452ef3fa52685c0a95a84e65187cc
AMO15310 <wachiraamos402@gmail.com>
2023-01-10 00:47:08 +0300

    Script to create a library of all c programs in your current directory

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files to .o files
-for file in *.c; do
-    gcc -c "$file"
-done
-
-# Create static library from .o files
-ar rcs liball.a *.o
+gcc -c *.c
+ar rc liball.a *.o
+ranlib liball.a
